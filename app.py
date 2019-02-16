542cf6cab2132d688335ca50d5139fd72f554920
Lucas May <lucasmayvieira@gmail.com>
2019-02-16 16:12:48 -0200

    getting temperature from US locations

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pyowm
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -6,11 +7,18 @@ from flask import render_template
 app = Flask(__name__)
 
 
+owm = pyowm.OWM('a583ecc7ffde60bc4a4ce437b0a6b3a4')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     template = 'index.html'
     if request.method == "POST":
         zipcode = request.form.get('zipcode')
+        observation = owm.weather_at_zip_code(zipcode=zipcode, country='US')
+        response = observation.get_weather()
+        temperature = response.get_temperature('celsius')
+        return render_template('result.html', temp=temperature['temp'])
     return render_template(template)
 
 
